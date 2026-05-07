f368adb88ea625dc400fde5aa3a873d8c92cc2c9
Luís Felício <luismjfelicio@gmail.com>
2026-05-07 18:47:59 +0200

    feat: first commit

---
+++ terraform/add-dev-user.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_user" "dev" {
+  name = "dev"
+}
+
+resource "aws_iam_policy" "dev_eks" {
+  name = "AmazonEKSDeveloperPolicy"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "eks:DescribeCluster",
+                "eks:ListClusters"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_user_policy_attachment" "dev_eks" {
+  user       = aws_iam_user.dev.name
+  policy_arn = aws_iam_policy.dev_eks.arn
+}
+
+resource "aws_eks_access_entry" "developer" {
+  cluster_name      = aws_eks_cluster.eks.name
+  principal_arn     = aws_iam_user.dev.arn
+  kubernetes_groups = ["my-viewer"]
+}

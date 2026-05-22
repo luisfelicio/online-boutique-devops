2f3bb46aa4ed9574e587be61e8a1e955c98e06c6
Luís Felício <luismjfelicio@gmail.com>
2026-05-23 00:42:52 +0200

    feat: ECR repo

---
+++ terraform/ecr.tf
@@ -0,0 +1,18 @@
+resource "aws_ecr_repository" "frontend" {
+  name                 = "frontend"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Environment = local.env
+    Project     = local.eks_name
+  }
+}
+
+output "ecr_repository_url" {
+  value       = aws_ecr_repository.frontend.repository_url
+  description = "The URL of the ECR repository for the frontend"
+}
